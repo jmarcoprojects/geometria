87abad0725094d97ed220de80df9adeb4b975aaf
Juan Antonio Marco <jmarcoprojects@gmail.com>
2020-02-03 22:48:03 +0100

    versión comentada

--- geometria/src/Rectangulo_JAM.java
+++ geometria/src/Rectangulo_JAM.java
@@ -1,20 +1,40 @@
-
+/**
+ * 
+ * @author Juan Antonio Marco
+ * @version 1.2
+ *
+ */
 public class Rectangulo_JAM extends FiguraGeometrica_JAM {
 	private double l1;
 	private double l2;
-	
+	/**
+	 * 
+	 * @param tipoFigura
+	 * @param lG
+	 * @param lP
+	 */
 	public Rectangulo_JAM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+
 	@Override
+	/**
+	 * Calcula el area del rectangulo
+	 * @return area
+	 * 
+	 */
 	public double area() {
 		return l1 * l2;
 	}
 	
 	@Override
+	/**
+	 * Calcula el perimetro del rectangulo
+	 * @return perimetro
+	 */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
