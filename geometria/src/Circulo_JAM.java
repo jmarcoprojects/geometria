9c0ff308a0a0440a0fb6d13946c3052966aae6ef
Juan Antonio Marco <jmarcoprojects@gmail.com>
2020-02-03 22:26:42 +0100

    versión refactorizada

--- geometria/src/Circulo_JAM.java
+++ geometria/src/Circulo_JAM.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_JAM extends FiguraGeometrica_JAM {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_JAM(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/Rectangulo_JAM.java
+++ geometria/src/Rectangulo_JAM.java
@@ -1,9 +1,9 @@
 
-public class Rectangulo extends FiguraGeometrica {
+public class Rectangulo_JAM extends FiguraGeometrica_JAM {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_JAM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
